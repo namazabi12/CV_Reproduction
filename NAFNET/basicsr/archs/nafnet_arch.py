16d2b8f2d3cf0cf35d31108d56a96b1fafd9e2f4
mmzb_razer <1970253762@qq.com>
2022-12-01 21:58:10 +0800

    upd nafnet

--- NAFNET/basicsr/archs/nafnet_arch.py
+++ NAFNET/basicsr/archs/nafnet_arch.py
@@ -2,7 +2,6 @@ import torch
 from torch import nn as nn
 import torch.nn.functional as F
 
-from basicsr.archs.arch_util import ResidualBlockNoBN, Upsample, make_layer
 from basicsr.utils.registry import ARCH_REGISTRY
 
 
@@ -12,11 +11,75 @@ class SimpleGate(nn.Module):
         return x1 * x2
 
 
+class BaselineBlock(nn.Module):
+    def __init__(self, c, DW_Expand=1, FFN_Expend=2, sca=False, sg=False):
+        super(BaselineBlock, self).__init__()
+        dw_channels = c * DW_Expand
+        ffn_channels = c * FFN_Expend
+
+        self.norm1 = nn.GroupNorm(1, c)
+        self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channels, kernel_size=1, stride=1, padding=0, groups=1,
+                               bias=True)
+        self.conv2 = nn.Conv2d(in_channels=dw_channels, out_channels=dw_channels, kernel_size=3, stride=1, padding=1,
+                               groups=dw_channels, bias=True)
+
+        if sg is True:
+            self.sg = SimpleGate()
+            self.p = 2
+        else:
+            self.sg = nn.GELU()
+            self.p = 1
+
+        if sca is True:
+            self.sca = nn.Sequential(
+                nn.AdaptiveAvgPool2d(1),
+                nn.Conv2d(in_channels=dw_channels // self.p, out_channels=dw_channels // self.p, kernel_size=1,
+                          stride=1, padding=0, groups=1, bias=True),
+            )
+        else:
+            self.sca = nn.Sequential(
+                nn.AdaptiveAvgPool2d(1),
+                nn.Conv2d(in_channels=dw_channels // self.p, out_channels=dw_channels // self.p, kernel_size=1,
+                          stride=1, padding=0, groups=1, bias=True),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(in_channels=dw_channels // self.p, out_channels=dw_channels // self.p, kernel_size=1,
+                          stride=1, padding=0, groups=1, bias=True),
+                nn.Sigmoid(),
+            )
+        self.conv3 = nn.Conv2d(in_channels=dw_channels // self.p, out_channels=c, kernel_size=1, stride=1, padding=0,
+                               groups=1, bias=True)
+
+        self.norm2 = nn.GroupNorm(1, c)
+        self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channels, kernel_size=1, stride=1, padding=0, groups=1,
+                               bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_channels // 2, out_channels=c, kernel_size=1, stride=1, padding=0,
+                               groups=1, bias=True)
+
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+
+    def forward(self, x):
+        x1 = self.norm1(x)
+        x1 = self.conv1(x1)
+        x1 = self.conv2(x1)
+        x1 = self.sg(x1)
+        x1 = x1 * self.sca(x1)
+        x1 = self.conv3(x1)
+        x1 = x1 * self.beta + x
+
+        x2 = self.norm2(x1)
+        x2 = self.conv4(x2)
+        x2 = self.sg(x2)
+        x2 = self.conv5(x2)
+        return x2 * self.gamma + x1
+
+
 class NAFBlock(nn.Module):
     def __init__(self, c, DW_Expand=2, FFN_Expend=2):
         super(NAFBlock, self).__init__()
         dw_channels = c * DW_Expand
         ffn_channels = c * FFN_Expend
+
         self.norm1 = nn.GroupNorm(1, c)
         self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channels, kernel_size=1, stride=1, padding=0, groups=1,
                                bias=True)
@@ -37,6 +100,8 @@ class NAFBlock(nn.Module):
         self.conv5 = nn.Conv2d(in_channels=ffn_channels // 2, out_channels=c, kernel_size=1, stride=1, padding=0,
                                groups=1, bias=True)
 
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
 
     def forward(self, x):
         x1 = self.norm1(x)
@@ -45,13 +110,13 @@ class NAFBlock(nn.Module):
         x1 = self.sg(x1)
         x1 = x1 * self.sca(x1)
         x1 = self.conv3(x1)
-        x1 = x1 + x
+        x1 = x1 * self.beta + x
 
         x2 = self.norm2(x1)
         x2 = self.conv4(x2)
         x2 = self.sg(x2)
         x2 = self.conv5(x2)
-        return x2 + x1
+        return x2 * self.gamma + x1
 
 
 @ARCH_REGISTRY.register()
@@ -122,8 +187,75 @@ class NAFNET(nn.Module):
         return F.pad(inp, (0, w_pad, 0, h_pad))
 
 
-if __name__ == "__main__":
-    net = NAFNET()
-    cri = nn.MSELoss()
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
+@ARCH_REGISTRY.register()
+class Baseline(nn.Module):
 
+    def __init__(self,
+                 img_channels=3,
+                 width=32,
+                 enc_block_nums=[2, 2, 4, 8],
+                 mid_block_nums=12,
+                 dec_block_nums=[2, 2, 2, 2],
+                 sca=False,
+                 sg=False):
+        super(Baseline, self).__init__()
+
+        self.begin = nn.Conv2d(img_channels, width, 3, 1, 1)
+
+        self.enc_block = nn.ModuleList()
+        self.down_block = nn.ModuleList()
+
+        self.sca = sca
+        self.sg = sg
+
+        ch = width
+        for num in enc_block_nums:
+            self.enc_block.append(nn.Sequential(*[BaselineBlock(ch, sca=self.sca, sg=self.sg) for _ in range(num)]))
+            self.down_block.append(nn.Conv2d(ch, ch * 2, 2, 2))
+            ch *= 2
+
+        self.mid_block = nn.Sequential(*[BaselineBlock(ch, sca=self.sca, sg=self.sg) for _ in range(mid_block_nums)])
+
+        self.dec_block = nn.ModuleList()
+        self.up_block = nn.ModuleList()
+        for num in dec_block_nums:
+            self.up_block.append(nn.Sequential(
+                nn.Conv2d(ch, ch * 2, 1, bias=False),
+                nn.PixelShuffle(2)
+            ))
+            ch //= 2
+            self.dec_block.append(nn.Sequential(*[BaselineBlock(ch, sca=self.sca, sg=self.sg) for _ in range(num)]))
+
+        self.end = nn.Conv2d(width, img_channels, 3, 1, 1)
+
+        self.mod = 2 ** len(enc_block_nums)
+
+    def forward(self, inp):
+        h, w = inp.shape[-2], inp.shape[-1]
+        inp = self.pad_to_pow(inp)
+        x = inp
+        x = self.begin(x)
+
+        skips = []
+
+        for enc, down in zip(self.enc_block, self.down_block):
+            x = enc(x)
+            skips.append(x)
+            x = down(x)
+
+        x = self.mid_block(x)
+
+        for dec, up, skip in zip(self.dec_block, self.up_block, skips[::-1]):
+            x = up(x)
+            x = x + skip
+            x = dec(x)
+
+        x = self.end(x)
+        x = x + inp
+        return x[:, :, :h, :w]
+
+    def pad_to_pow(self, inp):
+        h, w = inp.shape[-2], inp.shape[-1]
+        h_pad = (self.mod - h % self.mod) % self.mod
+        w_pad = (self.mod - w % self.mod) % self.mod
+        return F.pad(inp, (0, w_pad, 0, h_pad))
